a9b26ea055409c118488794d7ee92370f6cdc8ec
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-12-31 15:33:51 +0530

    creating two public subnets in our vpc

--- aws-vpc/output.tf
+++ aws-vpc/output.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
     value = aws_vpc.main.id
   
 }
+
+
+output "availability_zone_names" {
+  value = data.aws_availability_zones.available.names
+}
--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -24,3 +24,19 @@ resource "aws_internet_gateway" "main" {
   }
   )
 }
+
+#create two public subnets name should be roboshop-dev-us-east-1a and 1b
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  
+  cidr_block = var.public_subnets[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {
+    Name = "${var.project}-${var.environment}"
+  }
+  )
+}
