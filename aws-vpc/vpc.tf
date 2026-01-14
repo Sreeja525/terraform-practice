359d8bd7889f75f05bed7c8de3c70cdf7af28518
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2026-01-14 17:06:05 +0530

    corrected CIDR blocks for routes

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -158,14 +158,14 @@ resource "aws_route" "public" {
 
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = aws_eip.nat.id
-  gateway_id                = aws_nat_gateway.main.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id                = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = aws_eip.nat.id
-  gateway_id                = aws_nat_gateway.main.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id                = aws_nat_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
